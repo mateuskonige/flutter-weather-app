80e8b9f70c41136935b38b1205e9251dd544b75a
Mateus Reis <mateus.reis@com3telecom.com.br>
2026-02-03 12:04:15 -0300

    first commit

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("W E A T H E R")));
+  }
+}
